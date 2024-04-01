b50c34fcf0a702602dbd808cab0c022de4cf0af3
mandalriju698 <88059569+mandalriju698@users.noreply.github.com>
2024-04-01 20:28:23 +0530

    Update App.jsx

--- todo_App/vite-project/src/App.jsx
+++ todo_App/vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { NewTodoForm } from "./NewTodoForm"
 import "./styles.css"
 import { TodoList } from "./TodoList"
-//import {login} from "./login"
+
 
 export default function App() {
   const [todos, setTodos] = useState(() => {
